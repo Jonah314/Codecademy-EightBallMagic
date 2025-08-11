0809007989cbbed16aefe001228db56d461be5f1
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-08-11 13:53:56 -0500

    Added all My Css Styling, found a solution to center text on triangle

--- FrontEnd/Resources/JsFolder/index.js
+++ FrontEnd/Resources/JsFolder/index.js
@@ -1,20 +1,81 @@
+
+//Orginaly was going to connect back end to front end, but this assignment Requires Less
 //import {generateMessage} from '../../../../Codecademy-EightBallMagic/BackEnd/api.js';
 
+
+// functionality section
+/* -----------------------api.js Section ---------------- */
+
+// Create an Array of Future Predictions for the 8 Ball 
+const predictionsArray = ["It is Certain", "Yes, Definitely", "You may rely on it.", "Most likely", "Outlook good", 
+    "Don't count on it", "My reply is No", "Outlook no so good", "Ask Again Later", "Reply hazy, try again"];
+
+
+//Using this code to test whether or not the api.js is connecting to index.js
+const message = "Example Message";
+
+
+
+// Create a function the produces a random message from the array
+// The function should return a String
+
+function generateMessage(){
+    let index = Math.floor(Math.random()*10);
+    let pmessage = predictionsArray[index];
+    
+    return pmessage;
+}
+/*-----------------------------------------------------------*/
+
+
+
+// Used to Connect the buttion to .js
 const myButton = document.getElementById("myButton");
+// Used to put the message on the HTML Page
 const messageDisplay = document.getElementById("messageDisplay");
-//let generatedMessage = generateMessage();
 
-function createAlert(){
-    alert("Button was Clicked!");
-}
+// function connecting to "backend functionality"
 function retrieveMessage(){
-    messageDisplay.textContent = generatedMessage;
+    
+    messageDisplay.textContent = generateMessage();
+}
+// function for clearing messageDisplay
+function clearMessage(){
+    messageDisplay.textContent = '';
 }
 
 
-myButton.addEventListener("click", createAlert);
+//------- Adding Shaking Function ------- ///
+function shake(){
+    const shakingDiv = document.getElementById('shakingDiv');
+    shakingDiv.classList.add('shake-animation');
+    
+    // This section allows you to set the time for shake animation and resets it so you can do it again 
+    setTimeout(()=>{
+        shakingDiv.classList.remove('shake-animation');
+    },150);
+}
 
 
 
 
-//console.log(generatedMessage);
+
+// creating a function to combine all the functions with a timeout to space them out time wise
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve,ms));
+}
+
+async function realism(){
+    clearMessage();
+    shake();
+    await delay (500);
+    retrieveMessage();
+    
+}
+
+//when button is clicked it activates the function retrieveMessage
+myButton.addEventListener("click", realism);
+
+
+
+
