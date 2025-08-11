d9c8938175b5279f19acaae64c4e299a73fbc715
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-08-10 23:15:20 -0500

    I was having issues because I placed the javaScript Link in the head, The soliution was to move the link to the line above the closing body Tag. According to Meta Ai this is a common solution for my issue

--- FrontEnd/Resources/JsFolder/index.js
+++ FrontEnd/Resources/JsFolder/index.js
@@ -3,5 +3,9 @@ const myButton = document.getElementById("myButton");
 function createAlert(){
     alert("Button was Clicked!");
 }
+function retrieveMessage(){
+    
+}
+
 
 myButton.addEventListener("click", createAlert);
