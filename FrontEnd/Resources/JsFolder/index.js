e4cb5cdb50b0bf8df4254a6a2ef9ca6683e6d979
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-08-10 23:20:36 -0500

    Added logic in the index.js to make message appear from the index.js

--- FrontEnd/Resources/JsFolder/index.js
+++ FrontEnd/Resources/JsFolder/index.js
@@ -1,11 +1,12 @@
 const myButton = document.getElementById("myButton");
+const messageDisplay = document.getElementById("messageDisplay");
 
 function createAlert(){
     alert("Button was Clicked!");
 }
 function retrieveMessage(){
-    
+    messageDisplay.textContent = "You clicked the button !!!";
 }
 
 
-myButton.addEventListener("click", createAlert);
+myButton.addEventListener("click", retrieveMessage);
